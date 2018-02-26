fe7d661ebca28d3a9b29302103cf44a7b5d7d839
Sergiu <sergiu.munteanu@live.com>
2018-02-26 22:30:58 +0100

    Bob

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Text.RegularExpressions;
 
-public static class Bob
-{
+public static class Bob {
+
+    private static bool IsShouting(string input)
+    {
+        bool hasCaps = false;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] >= 'A' && input[i] <= 'Z')
+            {
+                hasCaps = true;                
+            }
+
+            if (input[i] >= 'a' && input[i] <= 'z')
+            {
+                return false;
+            }
+        }
+
+        return hasCaps;
+    }
+
+    private static bool IsQuestion(string input)
+    {
+        return input[input.Length - 1] == '?';
+    }
+
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        string prop = Regex.Replace(statement, @"\t|\n|\r", "");
+        prop = prop.Trim();
+        Console.WriteLine(prop);
+
+        if (prop.Length == 0)
+        {
+            return "Fine. Be that way!";
+        }
+
+        bool isQuestion = Bob.IsQuestion(prop);
+        bool isShouting = Bob.IsShouting(prop);
+
+        if (isShouting && isQuestion)
+        {
+            return "Calm down, I know what I'm doing!";
+        }
+
+        if (isShouting)
+        {
+            return "Whoa, chill out!";
+        }
+
+        if (isQuestion)
+        {
+            return "Sure.";
+        }
+
+        return "Whatever.";
     }
 }
