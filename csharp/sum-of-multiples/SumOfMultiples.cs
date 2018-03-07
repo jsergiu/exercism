ddc28c52e066d2daebaf855848ad0e3eeee9997e
Sergiu Munteanu <sergiu.munteanu@live.com>
2018-03-07 23:01:51 +0100

    Sum Of Multiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {		
+		IEnumerable<int> list = multiples.Where(x => x > 0);
+		if (list.Count() == 0)
+		{
+			return 0;
+		}
+
+		List<int> numbers = new List<int>();
+		int start = multiples.Min();
+
+		for (var i = start; i < max; i++)
+        {
+            bool isMatch = false;
+			isMatch = list.Any(x => x > 0 && i % x == 0);
+
+			if (isMatch)
+			{
+				numbers.Add(i);
+			}
+        }
+
+		return numbers.Sum();
+    }
+}
