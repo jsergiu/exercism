c0bd79107edb23bd920362d4548d9926c5a5f897
Sergiu Munteanu <sergiu.munteanu@live.com>
2018-03-08 00:43:22 +0100

    Collaz Conjecture

---
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+		if (number < 1)
+		{
+			throw new ArgumentException();
+		}
+
+		int steps = 0;
+		int value = number;
+
+		while (value > 1)
+		{
+			if (value % 2 == 0)
+			{
+				value = value / 2;
+			}
+			else
+			{
+				value = value * 3 + 1;
+			}
+			steps++;
+		}
+
+		return steps;
+    }
+}
